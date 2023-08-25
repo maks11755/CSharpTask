3998ac6ad1aff47570bad73e358c578677739eec
maksim <your>
2023-08-26 03:31:25 +1100

    Added task10

---
+++ task10/Program.cs
@@ -0,0 +1,22 @@
+﻿// task10: Напишите программу, 
+// которая принимает на вход трёхзначное
+// число и на выходе показывает 
+// вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0) number*=-1;
+
+if (number >= 100 && number <= 999)
+
+{
+ int lastDigit = (number % 100 - number % 10) / 10;
+ Console.WriteLine($"вторая цифра {lastDigit}");
+}
+
+else Console.WriteLine("Введено некорректное число");
+
