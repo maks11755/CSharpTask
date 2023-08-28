3281cb23742c13ea2696df646ac691011df5fedc
maksim <your>
2023-08-29 04:09:49 +1100

    Added task25

---
+++ task25/Program.cs
@@ -0,0 +1,31 @@
+﻿// task25. Напишите программу, которая принимает 
+// на вход два числа (A и B) и возводит число
+// A в натуральную степень B.
+// Алгоритм нужно реализовать самостоятельно, 
+// встроенную функцию использовать нельзя.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16 
+
+int numberA = ReadInt("Введите число A: ");
+int numberB = ReadInt("Введите число B: ");
+ToDegree(numberA, numberB);
+
+
+// Функция возведения в степень
+void ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
