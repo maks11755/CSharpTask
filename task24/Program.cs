f4a60490cf634f91ae42f5abb21bc79842ec718e
maksim <your>
2023-08-29 01:13:09 +1100

    Added task23

---
+++ task24/Program.cs
@@ -0,0 +1,25 @@
+﻿// Создайте программу, которая на 
+// вход принимает число (А) и выдает
+// сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+        sum += i; // sum = sum + i;
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = SumNumbers(number);
+Console.WriteLine($"Сумму чисел от 1 до {number} = {result}");
