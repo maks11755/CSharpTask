1b3fd8d9909418519ecab33b90789295352a81d4
maksim <your>
2023-09-04 00:28:39 +1100

    Added task43

---
+++ task43/Program.cs
@@ -0,0 +1,56 @@
+﻿// task43. Напишите программу, которая найдёт 
+// точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, 
+//k1, b2 и k2 задаются пользователем.
+//
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5).
+//
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double numberX = (b2 - b1)/(k1 - k2);
+double numberY = k2 * numberX + b2;
+
+Console.WriteLine($"Точка пересечения двух прямых: {numberX} : {numberY}");
+
+// // Метод ввода данных с консоли
+// double ReadData(string msg)
+// {
+//     Console.WriteLine(msg);
+//     return Convert.ToDouble(Console.ReadLine() ?? "0");
+// }
+
+// //Метод решает уравнение по Х
+// double xVolume(double b1, double k1, double b2, double k2)
+// {
+//     double xVol = (b2 - b1) / (k1 - k2);
+//     //double x = (b2 - b1) / (k1 - k2);
+//     return xVol;
+// }
+
+// // Метод решает уравнение по У
+// double yVolume(double b1, double k1, double x)
+// {
+//     double y = k1 * x + b1;
+//     return y;
+// }
+
+// // Вводим данные с консоли от пользователя
+// double b1 = ReadData("Введите значение b1: ");
+// double k1 = ReadData("Введите значение k1: ");
+// double b2 = ReadData("Введите значение b2: ");
+// double k2 = ReadData("Введите значение k2: ");
+
+// // Рассчитываем значение х и у
+// double x = xVolume(b1, k1, b2, k2);
+// double y = yVolume(b1, k1, x);
+
+// // Выводим ответ в консоль
+// Console.WriteLine("Значение Х = " + x);
+// Console.WriteLine("Значение Y = " + y);
