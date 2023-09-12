7fcc2b607b740822b4cac3db5baa73f2c186505f
maksim <your>
2023-09-12 18:23:23 +1100

    Added task55

---
+++ task55/Program.cs
@@ -0,0 +1,54 @@
+﻿// task55. Задайте двумерный массив. Напишите
+// программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна
+// вывести сообщение для пользователя.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        // Console.Write("[);
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],6}");
+        }
+        // Console.WriteLine("]);
+        Console.WriteLine();
+    }
+}
+
+void ReplaceRowsToColumns(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // for (int j = 0; j < i; j++)
+        {
+            if (j < i)  // <-delete
+            {
+                int temp = matrix[i, j];
+                matrix[i, j] = matrix[j, i];
+                matrix[j, i] = temp;
+            }
+        }
+    }
+}
+    int[,] array2d = CreateMatrixRndInt(3, 3, 1, 9);
+    PrintMatrix(array2d);
+    Console.WriteLine();
+    ReplaceRowsToColumns(array2d);
+    PrintMatrix(array2d);
