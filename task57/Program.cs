59db0911b77b0ebb0190820db7f73f313cf501fa
maksim <your>
2023-09-13 16:40:36 +1100

    Added task57

---
+++ task57/Program.cs
@@ -0,0 +1,85 @@
+﻿// task57. Составить частотный словарь элементов
+// двумерного массива. Частотный словарь содержит
+// информацию о том, сколько раз встречается элемент
+// входных данных.
+// набор данных {1, 9, 9, 0, 2, 8, 0, 9}
+// 0 is 2, 1 is 1, 2 is 1, 8 is 1, 9 is 3.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]},");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        // Console.Write("[);
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],6}");
+        }
+        //Console.WriteLine("]);
+        Console.WriteLine();
+    }
+}
+
+int[] MatrixToArray(int[,] matrix)
+{
+    int[] arr = new int[matrix.Length];
+    int k = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            arr[k++] = matrix[i, j];
+            // k++;
+        }
+    }
+    return arr;
+    
+}
+
+void DictionaryFrequency(int[] arr)
+{
+    int count = 1;
+    int num = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] == num) count++;
+        else
+        {
+            Console.WriteLine($" {num} -> {count}");
+            num = arr[i];
+            count = 1;
+        }
+    }
+    Console.WriteLine($" {num} -> {count}");
+}
+int[,] array2d = CreateMatrixRndInt(4, 4, 1, 9);
+PrintMatrix(array2d);
+int[] array = MatrixToArray(array2d);
+Array.Sort(array);
+PrintArray(array);
+DictionaryFrequency(array);
